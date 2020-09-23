2e42cc22b129ba27c9eb1447c1e62020285fd6e5
Manish Bansal <55635520+bansmani@users.noreply.github.com>
2020-09-23 10:38:42 +0530

    Update filewatcher.py
    added check for folder empty

--- filewatcher.py
+++ filewatcher.py
@@ -1,13 +1,24 @@
-
-from os import path
+from os import path, listdir
 import time
 
-def waitForFile(intervalInSec, durationInSec, name) :
-    start = time.time()
-    elapsedTime =0
 
-    while not path.exists(name) and elapsedTime < durationInSec :
+def waitForFile(intervalInSec, durationInSec, name):
+    start = time.time()
+    elapsedTime = 0
+
+    while not path.exists(name) and elapsedTime < durationInSec:
         time.sleep(intervalInSec)
         elapsedTime = int(time.time() - start)
 
     return path.exists(name)
+
+
+def waitUntilDirEmpty(intervalInSec, durationInSec, dir):
+    start = time.time()
+    elapsedTime = 0
+
+    while listdir(dir).__len__() <= 0 and elapsedTime < durationInSec:
+        time.sleep(intervalInSec)
+        elapsedTime = int(time.time() - start)
+
+    return listdir(dir).__len__() > 0
